3f2162740ce75304dc1fd3e59502f7c788d810f4
Harmin <harmin.parra-rueda@atos.net>
2023-08-24 01:03:13 +0200

    display horizontal line only for 'all' and 'manual' screenshots

--- src/pytest_selenium_auto/plugin.py
+++ src/pytest_selenium_auto/plugin.py
@@ -386,7 +386,8 @@ def pytest_runtest_makereport(item, call):
             return
 
         if (description is not None or exception_logged is True) \
-                and separator_display:
+                and separator_display is True\
+                and screenshots in ('all', 'manual'):
             extra.append(pytest_html.extras.html(f"<hr style='height:{separator_height};background-color:{separator_color}'>"))
 
         links = ""
