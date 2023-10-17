db802471605c3cf6694c04b3c337aed152515794
Harmin <harmin.parra-rueda@atos.net>
2023-10-17 09:30:56 +0200

    Add __all__ variable to plugin entrypoint module.

--- src/pytest_selenium_auto/__init__.py
+++ src/pytest_selenium_auto/__init__.py
@@ -1,2 +1,6 @@
+from .wrappers import CustomSelect as Select
+
 supported_browsers = ("firefox", "chrome", "chromium", "edge", "safari")
 screenshot_strategies = ('all', 'failed', 'last', 'manual', 'none')
+
+__all__ = ["Select"]
